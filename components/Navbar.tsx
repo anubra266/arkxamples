4fbb46d63d6e497d024976825028f58393eac75f
anubra266 <anubra266@gmail.com>
2023-03-23 17:50:11 +0100

    chore: add github link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,8 @@
+import { BsGithub } from "react-icons/bs";
+
 import { Flex, panda } from "design-system/jsx";
+
+import { IconButton } from "components/IconButton";
 
 import { ColorModeButton } from "./ColorModeButton";
 
@@ -14,7 +18,16 @@ export function Navbar() {
       <panda.span fontSize="lg" fontWeight="medium">
         <em>Ark</em>xamples
       </panda.span>
-      <Flex ml="auto">
+      <Flex ml="auto" gap="2">
+        <a href="https://github.com/anubra266/arkxamples" target="_blank">
+          <IconButton
+            aria-label="Open Github Repo"
+            title="Open Github Repo"
+            icon={<BsGithub />}
+            variant="tertiary"
+            size="xs"
+          />
+        </a>
         <ColorModeButton />
       </Flex>
     </Flex>
