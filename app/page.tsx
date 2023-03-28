b439432e1af267a0f3ef59497e255946a91937ba
anubra266 <anubra266@gmail.com>
2023-03-28 16:53:49 +0100

    chore: fix lint errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function Page() {
   return (
     <div>
       <p>
-        Don't have an idea for the homepage yet. Let's just get to the actual
-        thing:
+        Don&apos;t have an idea for the homepage yet. Let&apos;s just get to the
+        actual thing:
       </p>
       <a href="/checkbox">Get started</a>
     </div>
