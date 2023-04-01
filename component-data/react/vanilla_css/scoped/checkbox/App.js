e0a70cb1bafe7f699f2af88a371e3bb855d9542d
anubra266 <anubra266@gmail.com>
2023-04-01 23:50:54 +0100

    chore: refactor vanilla checkbox

--- component-data/react/vanilla_css/scoped/checkbox/App.js
+++ component-data/react/vanilla_css/scoped/checkbox/App.js
@@ -6,6 +6,8 @@ import {
 } from "@ark-ui/react";
 import React from "react";
 
+import CheckIcon from "./CheckIcon";
+import MinusIcon from "./MinusIcon";
 import "./styles.css";
 
 export default function Page() {
@@ -71,39 +73,3 @@ export default function Page() {
     </>
   );
 }
-
-const CheckIcon = () => (
-  <svg
-    width="14"
-    height="14"
-    viewBox="0 0 14 14"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M11.6666 3.5L5.24992 9.91667L2.33325 7"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
-const MinusIcon = () => (
-  <svg
-    width="14"
-    height="14"
-    viewBox="0 0 14 14"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M2.91675 7H11.0834"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
--- component-data/react/vanilla_css/scoped/checkbox/index.ts
+++ component-data/react/vanilla_css/scoped/checkbox/index.ts
@@ -4,7 +4,9 @@ export default {
   files: {
     "/styles.css": require("!!raw-loader!./styles.css").default,
     "/App.js": require("./App.js").default,
+    "/CheckIcon.js": require("./CheckIcon.js").default,
+    "/MinusIcon.js": require("./MinusIcon.js").default,
     ...sharedReactCode,
   },
-  visibleFiles: ["/App.js", "/styles.css"],
+  visibleFiles: ["/App.js", "/styles.css", "/CheckIcon.js", "/MinusIcon.js"],
 };
